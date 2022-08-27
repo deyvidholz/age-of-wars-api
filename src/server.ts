4aa8b6c14d9f84e5803f4216c8dad3ff805849f1
Deyvid Holz <deyvidholzt@gmail.com>
2022-08-27 05:55:08 -0300

    ♻️ add env copy

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,13 @@
 import { App } from './app';
+import fs from 'fs';
+import path from 'path';
+
+const envPath = path.join(__dirname, '..', '.env');
+const envExamplePath = path.join(__dirname, '..', '.env.example');
+
+if (!fs.existsSync(envPath)) {
+  fs.copyFileSync(envExamplePath, envPath);
+}
 
 const app = new App();
 
